6df0484cb936eb63b4750ac4bca75bd811138f37
Hugh Perkins <hughperkins@gmail.com>
2015-07-16 08:26:52 +0800

    update

--- useslua.c
+++ useslua.c
@@ -1,6 +1,7 @@
 #include "lua.h"
 
 void doSomething(lua_State *L) {
+  lua_newtable(L);
   lua_createtable(L, 1, 1);
 }
 
--- usesluacpp.cpp
+++ usesluacpp.cpp
@@ -3,6 +3,7 @@ extern "C" {
 }
 
 void doSomethingCpp(lua_State *L) {
+  lua_newtable(L);
   lua_createtable(L, 1, 1);
 }
 
