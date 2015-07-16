e04026330ada54769a15a3896fe3523e2b5d589c
Hugh Perkins <hughperkins@gmail.com>
2015-07-16 18:19:44 +0800

    builds on linux

--- usesluacpp.cpp
+++ usesluacpp.cpp
@@ -4,6 +4,9 @@ extern "C" {
   #include "lauxlib.h"
 }
 
+#include <iostream>
+using namespace std;
+
 void doSomethingCpp(lua_State *L) {
   lua_newtable(L);
   lua_createtable(L, 1, 1);
@@ -13,5 +16,6 @@ void usesluacpp_testLua() {
   lua_State *L = luaL_newstate();
   luaL_openlibs(L);
   lua_close(L);
+  cout << "usesluacpp_testLua()" << endl;
 }
 
